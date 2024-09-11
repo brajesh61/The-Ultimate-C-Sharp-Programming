15cb7139de9a68473801ae2b19152a3c2d95bd86
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-09-11 23:18:08 +0530

    Add files via upload

---
+++ Table/Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", input, i, i * input);
+            }
+        }
+    }
+}
