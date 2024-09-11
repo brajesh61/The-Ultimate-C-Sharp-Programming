77717ba6989825874eb8f2ff21f774f65ef44e6e
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-09-11 23:12:59 +0530

    Add files via upload

---
+++ Fizz Buzz Game/Fizz Buzz Game/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizz_Buzz_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*/Create a for loof from 1 to X (15)
+            3 & 5 = FizzBuzz
+            3 = Fizz
+            5 =  Buzz 
+            else = number
+             */
+            Console.Write("Enter Number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= input; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    } 
+}
