2d4ea2f9d1ac12d6fe3ddd7f39d9908c77627417
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-09-12 09:33:33 +0530

    Add files via upload

---
+++ Exercises/first character added  front & back/first character added  front & back/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_character_added__front___back
+{
+    internal class Program
+    {
+        //Write a C# program to create a string from a given string (length 1 or more) with the first character added at the front and back.
+        static void Main(string[] args)
+        {
+            Console.Write("Enter messagee: ");
+            String input = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(input.Substring(0,1) + input + "." + input.Substring(0,1));
+            Console.ReadLine();
+        }
+    }
+}
