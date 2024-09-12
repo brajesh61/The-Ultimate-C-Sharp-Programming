5962d6813589ba84802bba5278e8a1548d2c6cfd
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-09-12 09:24:12 +0530

    Add files via upload

---
+++ Exercises/Exchange String/Exchange String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange_String
+{
+    internal class Program
+    {
+        //Write a C# program to create a new string from a given string where the first and last characters change their positions. 
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string  input = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(input.Substring(input.Length - 1) + input.Substring(1) + input.Substring(0,1));
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises/specified character from a non-empty/specified character from a non-empty/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specified_character_from_a_non_empty
+{
+    internal class Program
+    {
+        //Write a C# program that removes a specified character from a non-empty string using the index of a character.
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String: ");
+            string input = Convert.ToString(Console.ReadLine());
+
+            //Reverse
+            for (int i = input.Length; i >= 0; i--)
+            {
+                Console.WriteLine(input.Substring(i));
+
+            }
+            //stright
+            Console.WriteLine();
+            for (int i = 0; i < input.Length;  i++)
+            {
+                Console.WriteLine(input.Substring(i));
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
