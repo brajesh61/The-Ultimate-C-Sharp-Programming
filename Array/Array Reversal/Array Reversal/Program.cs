6d2ed9af70b90127468bbd7eab7414dfa9b6fcc1
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-09-18 08:16:39 +0530

    Add files via upload

---
+++ Array/Array Reversal/Array Reversal/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Reversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                3, 9, 4, 6, 7, 2, 1, 0, 5, 8, 10
+            };
+
+            Array.Reverse(numbers); //reverse print not sorting
+            foreach (int i in numbers)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            Array.Sort(numbers);// Low to high
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+
+            //Print revers: Hight to low
+            int[] SortedNum = new int[numbers.Length];
+            int x = 0;
+            for (int i = numbers.Length - 1; i >= 0;  i--)
+            {
+                SortedNum[x] = numbers[i];
+                x++;
+                    
+            }
+
+            foreach (var num2 in SortedNum)
+            {
+                Console.Write($"{num2} ");
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Array/Array Sorting/Array Sorting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                4, 4, 5, 10, 8, 6, 3, 2, 1, 7, 0
+            };
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+
+            //sorting
+            Array.Sort(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
