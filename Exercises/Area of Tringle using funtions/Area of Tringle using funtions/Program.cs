a936c3403f5751754298168e840abf1ba48ef36c
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-10-01 11:03:38 +0530

    Add files via upload

---
+++ Exercises/Area of Tringle using funtions/Area of Tringle using funtions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_Tringle_using_funtions
+{
+    internal class Program
+    {
+        /*
+         * Ask user for width and heigth, store them
+         * create funtion to calculate the are 
+         * funtion should calculate the are using : (Width * Heigth) / 2
+         * call in main funtion and print out the area of tringle
+         */
+
+        static void Main(string[] args)
+        {
+            double width = ReadInt("Enter width: ");
+            double heigth = ReadInt("Enter heigth: ");
+
+            double result = CalArea(width, heigth);
+            Console.WriteLine($"Area of tringle is {result}.");
+
+            Console.ReadLine();
+        }
+
+        static double CalArea(double width, double heigth)
+        {
+            return Convert.ToDouble((width * heigth) / 2);
+        }
+        static int ReadInt(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
