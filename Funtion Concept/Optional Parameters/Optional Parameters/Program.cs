8c3b240c96dbf613967c9485fb6a5f6e71d90914
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-10-01 11:02:51 +0530

    Add files via upload

---
+++ Funtion Concept/Optional Parameters/Optional Parameters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Add(5);
+            Console.WriteLine(result);
+
+            int result1 = Add(500);
+            Console.WriteLine(result1);
+
+             NamePrint("Brajesh");
+            
+
+            Console.ReadLine();
+        }
+
+        static int Add(int a, int b = default)
+        {
+            return a + b; 
+        }
+
+        static int AddInt(int a, [Optional] int b )
+        {
+            return a + b;
+        }
+
+        static void NamePrint(String name = "Amrita")
+        {
+            Console.WriteLine($"My name is {name}.");
+        }
+    }
+}
