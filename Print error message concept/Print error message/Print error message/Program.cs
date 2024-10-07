5f4062b60714df19fabe5bfd04bab9616b2e19e1
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-10-07 22:36:55 +0530

    Add files via upload

---
+++ Print error message concept/Print error message/Print error message/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_error_message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop)
+            {
+                try
+                {
+                    Console.Write("Enter number: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(num);
+                    loop = false;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+            }
+        }
+    }
+}
