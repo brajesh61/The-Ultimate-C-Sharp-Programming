b5de7e58f0cae25c069f3469e33a2ade2b063f3e
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-10-08 13:28:16 +0530

    Add files via upload

---
+++ Exercises/Fix logic error/Fix logic error/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_logic_error
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 35;
+            if(age > 18 && age < 30)
+            {
+                Console.WriteLine("Over 18");
+            }
+            else if(age > 30)
+            {
+                Console.WriteLine("Over 30");
+            }
+            else if (age > 50)
+            {
+                Console.WriteLine("Over 50");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
