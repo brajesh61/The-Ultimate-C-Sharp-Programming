cba138d90ff557607c89b378c23cd040c91bf2fd
Brajesh Kumar <brajeshkumarrds@gmail.com>
2024-10-08 13:35:56 +0530

    Add files via upload

---
+++ Exercises/Reverse the word/Reverse the word/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_the_word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user to enter a word
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            // Convert the word into a character array, reverse it, and then convert back to a string
+            char[] wordArray = word.ToCharArray();
+            Array.Reverse(wordArray);
+            string reversedWord = new string(wordArray);
+
+            // Output the reversed word
+            Console.WriteLine("Reversed word: " + reversedWord);
+        }
+    }
+}
